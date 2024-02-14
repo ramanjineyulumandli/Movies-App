41e25867cb25e54b3b760ab7072f3f0c579b6877
ramanjineyulumandli <ramanjineyulumandli99@gmail.com>
2024-02-14 07:39:32 +0000

    Movies App

---
+++ src/components/Popular/index.js
@@ -0,0 +1,31 @@
+import {Component} from 'react'
+import Header from '../Header'
+
+import MovieContext from '../../context/MovieContext'
+import Footer from '../Footer'
+import PopularItem from '../PopularItem'
+
+class Popular extends Component {
+  render() {
+    return (
+      <MovieContext.Consumer>
+        {value => {
+          const {username} = value
+          console.log('username from Home', {username})
+
+          return (
+            <>
+              <div className="home-container" testid="popular">
+                <Header />
+                <PopularItem />
+                <Footer />
+              </div>
+            </>
+          )
+        }}
+      </MovieContext.Consumer>
+    )
+  }
+}
+
+export default Popular
